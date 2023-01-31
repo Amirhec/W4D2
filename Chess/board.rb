a039bae766addfc578f7cf66871ceab1fc00c81c
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-01-31 16:44:12 -0500

    add class and one function

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,6 +1,8 @@
 
 class Board
 
+    attr_reader :board
+
     def initialize(board)
         @board = Array.new(8) { Array.new(8) }
     end
@@ -16,7 +18,34 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
+        row_start, col_start = start_pos
+        row_end,col_end = end_pos
+            if @board[row_start][col_start].is_a?(Nullpiece) 
+                raise "No piece at start pos"
+
+            else
+                raise "The piece cannot move to end_pos"
+
+            end
 
     end
+
+end
+
+class Piece
+    def initialize(value,pos)
+        @value = value
+        @pos = pos
+    end
+end
+
+class Nullpiece < Piece
+    def initialize(value,pose)
+        super
+        @value = nil
+    end
+    
+
+    
 
 end
