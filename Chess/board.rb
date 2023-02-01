6505e5aa4c023dfe478a7df4648398ae350e2f18
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-02-01 14:56:31 -0500

    board updated

--- Chess/board.rb
+++ Chess/board.rb
@@ -14,30 +14,60 @@ class Board
     def initialize
         @board = Array.new(8) { Array.new(8) }
 
-        queen = Queen.new("queen", [0,3])
-        king = King.new("king", [0,4])
-        bishop_1 = Bishop.new("bishop",[0,2])
-        bishop_2 = Bishop.new("bishop", [0,5])
-        knight_1 = Knight.new("knight", [0,6])
-        knight_2 = Knight.new("knight", [0,1])
-        rook_1 = Rook.new("rook", [0,0])
-        rook_2 = Rook.new("rook", [0,7])
+        @queen_black = Queen.new("queen", [0,3])
+        king_black= King.new("king", [0,4])
+        bishop_1_black = Bishop.new("bishop",[0,2])
+        bishop_2_black = Bishop.new("bishop", [0,5])
+        knight_1_black = Knight.new("knight", [0,6])
+        knight_2_black = Knight.new("knight", [0,1])
+        rook_1_black = Rook.new("rook", [0,0])
+        rook_2_black = Rook.new("rook", [0,7])
 
-        pawn_1 = Pawn.new("pawn", [1,0])
-        pawn_2 = Pawn.new("pawn", [1,1])
-        pawn_3 = Pawn.new("pawn", [1,2])
-        pawn_4 = Pawn.new("pawn", [1,3])
-        pawn_5 = Pawn.new("pawn", [1,4])
-        pawn_6 = Pawn.new("pawn", [1,5])
-        pawn_7 = Pawn.new("pawn", [1,6])
-        pawn_8 = Pawn.new("pawn", [1,7])
+        pawn_1_black = Pawn.new("pawn", [1,0])
+        pawn_2_black = Pawn.new("pawn", [1,1])
+        pawn_3_black = Pawn.new("pawn", [1,2])
+        pawn_4_black = Pawn.new("pawn", [1,3])
+        pawn_5_black = Pawn.new("pawn", [1,4])
+        pawn_6_black = Pawn.new("pawn", [1,5])
+        pawn_7_black = Pawn.new("pawn", [1,6])
+        pawn_8_black = Pawn.new("pawn", [1,7])
 
-        ary = [queen, king, bishop_1, bishop_2, knight_1, knight_2, rook_1, rook_2, pawn_1, pawn_2, pawn_3, pawn_4, pawn_5, pawn_6, pawn_7, pawn_8]
+
+
+        
+        queen_white = Queen.new("queen", [7,3])
+        king_white = King.new("king", [7,4])
+        bishop_1_white = Bishop.new("bishop",[7,2])
+        bishop_2_white = Bishop.new("bishop", [7,5])
+        knight_1_white = Knight.new("knight", [7,1])
+        knight_2_white = Knight.new("knight", [7,6])
+        rook_1_white = Rook.new("rook", [7,0])
+        rook_2_white = Rook.new("rook", [7,7])
+
+        pawn_1_white = Pawn.new("pawn", [6,0])
+        pawn_2_white = Pawn.new("pawn", [6,1])
+        pawn_3_white = Pawn.new("pawn", [6,2])
+        pawn_4_white = Pawn.new("pawn", [6,3])
+        pawn_5_white = Pawn.new("pawn", [6,4])
+        pawn_6_white = Pawn.new("pawn", [6,5])
+        pawn_7_white = Pawn.new("pawn", [6,6])
+        pawn_8_white = Pawn.new("pawn", [6,7])
+
+        ary = [queen_black, king_black, bishop_1_black, bishop_2_black, knight_1_black, knight_2_black, rook_1_black, rook_2_black, pawn_1_black, pawn_2_black, pawn_3_black, pawn_4_black, pawn_5_black, pawn_6_black, pawn_7_black, pawn_8_black]
 
         ary.each do |instance|
             # debugger
             self[instance.pos] = instance.value
         end
+
+
+        ary1 = [queen_white, king_white, bishop_1_white, bishop_2_white, knight_1_white, knight_2_white, rook_1_white, rook_2_white, pawn_1_white, pawn_2_white, pawn_3_white, pawn_4_white, pawn_5_white, pawn_6_white, pawn_7_white, pawn_8_white]
+
+        ary1.each do |instance|
+            self[instance.pos] = instance.value 
+
+        end
+
 
     end
 
@@ -61,6 +91,13 @@ class Board
                 raise "The piece cannot move to end_pos"
 
             end
+
+        instance = @board[start_pos]
+        @board[end_pos] = instance
+
+
+
+        
 
     end
 
